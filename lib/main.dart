57b81f84ca19df551684de57e4f69954a392fc61
Eman Nasef <55794743+EmanNasef@users.noreply.github.com>
2022-03-18 18:18:43 +0200

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rescue/shared/cubit/cubit_doctor/cubit.dart';
+import 'package:rescue/shared/cubit/cubit_doctor/state.dart';
+import 'modules/welcome_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => DoctorCubit()),
+      ],
+      child: BlocConsumer<DoctorCubit, DoctorStates>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return MaterialApp(
+            title: 'Rescuer',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: const WelcomeScreen(),
+          );
+        },
+      ),
+    );
+  }
+}
